3eb661d5d71a1d98de647ad61508a725ba176b2a
Aidan Walden <44129920+aidan-walden@users.noreply.github.com>
2026-02-06 21:56:56 -0500

    Add: Products Of Array Discluding Self (Medium)

---
+++ Medium/products-of-array-discluding-self.py
@@ -0,0 +1,38 @@
+#
+# Problem: Products Of Array Discluding Self
+# Difficulty: Medium
+# URL: https://neetcode.io/problems/products-of-array-discluding-self
+# Date: 2026-02-07
+#
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        output = []
+
+        memo_left = {}
+        memo_right = {}
+
+        for i, num in enumerate(nums):
+
+            def find_product_left(index):
+                if index < 0:
+                    return 1
+                if index in memo_left:
+                    return memo_left[index]
+                elif index == 1:
+                    return nums[0] * nums[1]
+                elif index == 0:
+                    return nums[0]
+                
+                result = nums[index] * find_product_left(index - 1)
+                memo_left[index] = result
+                return result
+
+            def find_product_right(index):
+                if index > len(nums) - 1:
+                    return 1
+                if index in memo_right:
+                    return memo_right[index]
+                elif index == len(nums) - 2:
+                    return nums[-1] * nums[-2]
+                elif index == len(nums) - 1:
