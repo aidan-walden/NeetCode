7e769914fb366285cffeb93fdaba166274671bc3
Aidan Walden <44129920+aidan-walden@users.noreply.github.com>
2026-02-06 21:57:45 -0500

    The extension is oh so very broken

--- Medium/products-of-array-discluding-self.py
+++ Medium/products-of-array-discluding-self.py
@@ -5,34 +5,44 @@
 # Date: 2026-02-07
 #
 
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        output = []
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        output = []
 
-        memo_left = {}
-        memo_right = {}
+        memo_left = {}
+        memo_right = {}
 
-        for i, num in enumerate(nums):
+        for i, num in enumerate(nums):
 
-            def find_product_left(index):
-                if index < 0:
-                    return 1
-                if index in memo_left:
-                    return memo_left[index]
-                elif index == 1:
-                    return nums[0] * nums[1]
-                elif index == 0:
-                    return nums[0]
-                
-                result = nums[index] * find_product_left(index - 1)
-                memo_left[index] = result
-                return result
+            def find_product_left(index):
+                if index < 0:
+                    return 1
+                if index in memo_left:
+                    return memo_left[index]
+                elif index == 1:
+                    return nums[0] * nums[1]
+                elif index == 0:
+                    return nums[0]
+                
+                result = nums[index] * find_product_left(index - 1)
+                memo_left[index] = result
+                return result
 
-            def find_product_right(index):
-                if index > len(nums) - 1:
-                    return 1
-                if index in memo_right:
-                    return memo_right[index]
-                elif index == len(nums) - 2:
-                    return nums[-1] * nums[-2]
-                elif index == len(nums) - 1:
+            def find_product_right(index):
+                if index > len(nums) - 1:
+                    return 1
+                if index in memo_right:
+                    return memo_right[index]
+                elif index == len(nums) - 2:
+                    return nums[-1] * nums[-2]
+                elif index == len(nums) - 1:
+                    return nums[-1]
+
+                result = nums[index] * find_product_right(index + 1)
+                memo_right[index] = result
+                return result
+
+            this_product = find_product_left(i - 1) * find_product_right(i + 1)
+            output.append(this_product)
+
+        return output
