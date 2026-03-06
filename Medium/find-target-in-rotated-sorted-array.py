c61b36a320dfb711daf4fac003fcb17f4954c3a6
Aidan Walden <44129920+aidan-walden@users.noreply.github.com>
2026-03-06 14:20:19 -0500

    Add solution for find-target-in-rotated-sorted-array

---
+++ Medium/find-target-in-rotated-sorted-array.py
@@ -0,0 +1,31 @@
+#
+# Problem: Search in Rotated Sorted Array
+# Difficulty: Medium
+# URL: https://neetcode.io/problems/find-target-in-rotated-sorted-array
+# Date: 2026-03-06
+#
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+
+        while left <= right:
+            middle = left + ((right - left) // 2)
+
+            if nums[middle] == target:
+                return middle
+
+            if nums[left] <= nums[middle]:
+                if nums[left] <= target <= nums[middle]:
+                    right = middle - 1
+                else:
+                    left = middle + 1
+            elif nums[middle] <= nums[right]:
+                if nums[middle] <= target <= nums[right]:
+                    left = middle + 1
+                else:
+                    right = middle - 1
+                    
+        return -1
