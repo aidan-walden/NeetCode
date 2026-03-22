43d765e805deb33c1cc60bc733f28da4cb29f886
Aidan Walden <44129920+aidan-walden@users.noreply.github.com>
2026-03-22 17:51:43 -0400

    Fix redundant method duplication in valid binary search tree

--- Medium/valid-binary-search-tree.py
+++ Medium/valid-binary-search-tree.py
@@ -13,7 +13,7 @@
 #         self.right = right
 
 class Solution:
-    def checkLeftSubtree(self, min_value: Optional[int], max_value: Optional[int], root: Optional[TreeNode]) -> bool:
+    def checkSubtree(self, min_value: Optional[int], max_value: Optional[int], root: Optional[TreeNode]) -> bool:
         if not root:
             return True
         if min_value is not None and root.val <= min_value:
@@ -21,21 +21,8 @@ class Solution:
         if max_value is not None and root.val >= max_value:
             return False
 
-        leftValid = self.checkLeftSubtree(min_value, root.val, root.left) if root.left else True
-        rightValid = self.checkRightSubtree(root.val, max_value, root.right) if root.right else True
-
-        return leftValid and rightValid
-
-    def checkRightSubtree(self, min_value: Optional[int], max_value: Optional[int], root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
-        if min_value is not None and root.val <= min_value:
-            return False
-        if max_value is not None and root.val >= max_value:
-            return False
-
-        leftValid = self.checkLeftSubtree(min_value, root.val, root.left) if root.left else True
-        rightValid = self.checkRightSubtree(root.val, max_value, root.right) if root.right else True
+        leftValid = self.checkSubtree(min_value, root.val, root.left) if root.left else True
+        rightValid = self.checkSubtree(root.val, max_value, root.right) if root.right else True
 
         return leftValid and rightValid
 
@@ -44,8 +31,8 @@ class Solution:
         if not root:
             return True
 
-        leftValid = self.checkLeftSubtree(None, root.val, root.left) if root.left else True
-        rightValid = self.checkRightSubtree(root.val, None, root.right) if root.right else True
+        leftValid = self.checkSubtree(None, root.val, root.left) if root.left else True
+        rightValid = self.checkSubtree(root.val, None, root.right) if root.right else True
 
         return leftValid and rightValid
 
